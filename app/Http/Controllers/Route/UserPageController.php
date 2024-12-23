940b0441eeb2a26ba891e7a45a29f2486a011b5b
laravel2004 <nug.agung0106@gmail.com>
2024-12-23 19:39:41 +0700

    merge

--- app/Http/Controllers/Route/UserPageController.php
+++ app/Http/Controllers/Route/UserPageController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Route;
 
 use App\Http\Controllers\Controller;
+use App\Models\Review;
 use Illuminate\Http\Request;
 
 class UserPageController extends Controller
 {
     public function index()
     {
-        return view('pages.home.index');
+        $reviews = Review::where('is_approved', true)->get();
+        return view('pages.home.index', compact('reviews'));
     }
 }
